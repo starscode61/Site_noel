16dbec863dbd39f627c785058afa4fff1366bf47
starscode61 <164739585+starscode61@users.noreply.github.com>
2025-10-11 20:48:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,15 +24,34 @@ function hidePopup() {
     document.body.style.overflow = "auto";
 }
 
-ok.addEventListener("click", () => {
-    hidePopup();
-    scheduleNextPopup();
-});
 
-function scheduleNextPopup() {
-    const delay = (5 + Math.random() * 5) * 1000;
+function NextPopup() {
+    const delay = (7 + Math.random() * 3) * 1000;
     setTimeout(showPopup, delay);
 }
 
+ok.addEventListener("click", () => {
+    hidePopup();
+    NextPopup();
+});
+
 showPopup();
 
+const curseur = document.getElementById("curseur");
+document.addEventListener("mousemove", e => {
+    curseur.style.left = e.pageX + "px";
+    curseur.style.top = e.pageY + "px";
+});
+
+function majCompteur() {
+    const compteur = document.getElementById("compteur");
+    const now = new Date();
+    const year = now.getFullYear();
+    const christmas = new Date(year, 11, 25);
+    if (now > christmas) christmas.setFullYear(year + 1);
+    const diff = christmas - now;
+    const jours = Math.ceil(diff / (1000 * 60 * 60 * 24));
+    compteur.textContent = `Il reste ${jours} jour${jours > 1 ? "s" : ""} avant Noël !`;
+}
+majCompteur();
+setInterval(majCompteur, 3600000);
