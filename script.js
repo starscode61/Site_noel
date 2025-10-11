772aa5ebc1fb2e9f9ae0eaa1c4e51800eeb0af5e
heka39 <thibaultmalard55@gmail.com>
2025-10-11 19:00:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const box = document.getElementById("flocon-box")
 
+function create_flocon() {
+    const flocon = document.createElement("div")
+    flocon.classList.add("flocon")
+    flocon.textContent = "❄"
+    let positonne = Math.random() * 100
+    flocon.style.left = positonne + "vw"
+    
+    box.appendChild(flocon)
+
+    setTimeout(() => flocon.remove(),5000 )
+}
+
+setInterval(create_flocon,100)
