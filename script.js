2f13eaa3c624916ac8fb6bf3c9126f3f0f1ce869
heka39 <thibaultmalard55@gmail.com>
2025-10-12 01:18:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const flocon_box = document.getElementById("flocon-box");
+const curseur = document.getElementById("curseur");
+
+
+
+let mousex = 0
+let mousey = 0
+document.addEventListener("mousemove", function(e){
+    mousex = e.pageX
+    mousey = e.pageY
+    curseur.style.left = e.pageX + "px";
+    curseur.style.top = e.pageY + "px";
+})
+
+
+
 
 function create_flocon() {
     const flocon = document.createElement("div");
@@ -25,23 +40,20 @@ function hidePopup() {
 }
 
 
-function NextPopup() {
+/*function NextPopup() {
     const delay = (7 + Math.random() * 3) * 1000;
     setTimeout(showPopup, delay);
-}
+}*/
 
 ok.addEventListener("click", () => {
     hidePopup();
-    NextPopup();
+    create_follow()
 });
 
-showPopup();
 
-const curseur = document.getElementById("curseur");
-document.addEventListener("mousemove", e => {
-    curseur.style.left = e.pageX + "px";
-    curseur.style.top = e.pageY + "px";
-});
+
+
+
 
 function majCompteur() {
     const compteur = document.getElementById("compteur");
@@ -55,3 +67,53 @@ function majCompteur() {
 }
 majCompteur();
 setInterval(majCompteur, 3600000);
+
+const follow_box = document.getElementById("follow_curseur")
+const vitesse = 3
+
+
+
+
+function follow(objet) {
+    let objetx = objet.offsetLeft
+    let objety = objet.offsetTop
+        objetx = objet.offsetLeft
+        objety = objet.offsetTop
+        let a = mousex - objetx
+        let b = mousey - objety
+    let distance = Math.sqrt(Math.pow(a,2)+Math.pow(b,2))
+
+    if (distance > 10 ) {
+        let ratio = distance/vitesse
+        objet.style.left = objetx + a/ratio + "px"
+        objet.style.top = objety + b/ratio + "px"
+        requestAnimationFrame(() => follow(objet))
+    }
+    else {
+        showPopup()
+        objet.remove()
+        console.log("a");
+        
+    }
+
+    
+}
+
+
+function create_follow() {
+    const objet = document.createElement("div")
+    objet.textContent = "🛷"
+    objet.style.position = "absolute"
+    objet.style.fontSize = "40px"
+    objet.style.left = 0
+    objet.style.top = 0
+    document.body.appendChild(objet)
+    
+    requestAnimationFrame(() => follow(objet))
+    console.log("a");
+    
+    
+}
+
+
+showPopup()
