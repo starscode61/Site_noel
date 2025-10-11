53fb9d2d247bba79bf33f12bd83a954409077348
starscode61 <164739585+starscode61@users.noreply.github.com>
2025-10-11 19:30:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ function create_flocon() {
 }
 
 setInterval(create_flocon,100)
+
+
+
+const popup = document.getElementById("popup");
+const ok = document.getElementById("ok");
+
+function showPopup() {
+    popup.style.display = "flex";
+    document.body.style.overflow = "hidden";
+}
+
+function hidePopup() {
+    popup.style.display = "none";
+    document.body.style.overflow = "auto";
+}
+
+ok.addEventListener("click", () => {
+    hidePopup();
+    scheduleNextPopup();
+});
+
+function scheduleNextPopup() {
+    const delay = (5 + Math.random() * 5) * 1000;
+    setTimeout(showPopup, delay);
+}
+
+showPopup();
