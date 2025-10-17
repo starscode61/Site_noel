6342ef5c556c768ae02567af866eaed9fab9df58
heka39 <thibaultmalard55@gmail.com>
2025-10-17 12:59:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const flocon_box = document.getElementById("flocon-box");
+const curseur = document.getElementById("curseur");
 
 let mousex = 0
 let mousey = 0
 document.addEventListener("mousemove", function(e){
+    curseur.style.left = e.pageX + "px";
+    curseur.style.top = e.pageY + "px";
     mousex = e.pageX
     mousey = e.pageY
 })
