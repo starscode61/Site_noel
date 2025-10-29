7a44344f90b3d8283be5bf3d527ac7364ae717a9
heka39 <thibaultmalard55@gmail.com>
2025-10-29 12:35:35 +0100

    Add files via upload

---
+++ flags.js
@@ -0,0 +1,37 @@
+let all_flags = null
+
+fetch("flags.json")
+    .then(res => res.json())
+    .then(data => {
+        all_flags = data;
+        for (let i = 0; i < 5; i++){
+            const div = document.createElement("div")
+            div.id = i
+            document.body.appendChild(div)
+            
+            if (i % 2 === 0) {
+                setInterval(() => create_flag("left", i * 15 + 20, document.getElementById(i)), 1000);
+            } else {
+                setInterval(() => create_flag("right", i * 15 + 20, document.getElementById(i)), 1000);
+            }
+            
+        }
+           
+        
+    })
+
+
+function create_flag(direction, position, box) {
+    const little_box = document.createElement("div");
+    const number = Math.floor(Math.random() * all_flags.length);
+    const img = document.createElement("img")
+    img.src ="assets/flags/" + all_flags[number]["code"] + ".svg"
+    const text = document.createElement("div")
+    text.textContent = all_flags[number]["texte"]
+    little_box.appendChild(img)
+    little_box.appendChild(text)
+    little_box.classList.add(direction);
+    little_box.style.top = position + "vh";
+    box.appendChild(little_box);
+    setTimeout(() => little_box.remove(), 15000);
+}
